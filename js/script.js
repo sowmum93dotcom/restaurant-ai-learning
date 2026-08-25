208bcb9a64f0e9f74cf555da59e2d7f6f1e23f3d
sowmum93dotcom <sowmum93@gmail.com>
2026-08-25 19:07:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     try {
 
-      const response = await fetch("/api/api/generate", {
+      const response = await fetch("/api/generate", {
 
         method: "POST",
 
